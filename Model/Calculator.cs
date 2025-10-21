03dee8797b3e09f649968c878241c6b5a3066d42
NEROAT <kikahi19@gmail.com>
2025-10-21 09:57:44 +0900

    초기 커밋.

---
+++ Model/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Model
+{
+    internal class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y) 
+        {
+            return x * y; 
+        }
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+        public double Calculate(double x, double y, string operation)
+        {
+            switch(operation)
+            {
+                case "+":
+                    return Add(x, y);
+                case "-":
+                    return Subtract(x, y);
+                case "*":
+                    return Multiply(x, y);
+                case "/":
+                    return Divide(x, y);
+                default:
+                    throw new Exception();
+            }
+        }
+
+    }
+}
---
+++ ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.ViewModel
+{
+    internal class CalculatorViewModel
+    {
+        private string _numberDisplay;
+        private double _inputNumber;
+        private string _operation;
+        private bool _readyNumber;
+
+        public CalculatorViewModel()
+        {
+            _numberDisplay = "0";
+            _readyNumber = true;
+        }
+
+        public void NumberPressed(string number)
+        {
+            if (_readyNumber || _numberDisplay == "0")
+            {
+                _numberDisplay = number;
+                _readyNumber = false;
+            }
+            else
+            {
+                _numberDisplay += number;
+            }
+        }
+
+        public void OperationPressed(string operation)
+        {
+            
+        }
+
+        public void EqualsPressed()
+        {
+
+        }
+
+        public void ClearPressed()
+        {
+
+        }
+    }
+
+    
+}
