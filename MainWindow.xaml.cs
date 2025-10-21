6400a67c2e0e7d512a4ea62f5132a839f7698fdc
NEROAT <kikahi19@gmail.com>
2025-10-21 11:13:52 +0900

    feat : 사칙연산 기능 개발

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Calc.ViewModel;
 
 namespace Calc
 {
@@ -16,9 +17,36 @@ namespace Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        CalculatorViewModel viewModel;
         public MainWindow()
         {
             InitializeComponent();
+            viewModel = new CalculatorViewModel();
+            DataContext = viewModel;
+
         }
+        void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string number = button.Content.ToString();
+            viewModel.NumberPressed(number);
+        }
+        void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            viewModel.ClearPressed();
+        }
+        void OperationButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string operation = button.Content.ToString();
+            viewModel.OperationPressed(operation);
+        }
+        void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            viewModel.EqualsPressed();
+        }
+
     }
 }
--- ViewModel/CalculatorViewModel.cs
+++ ViewModel/CalculatorViewModel.cs
@@ -1,50 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calc.Model;
 
 namespace Calc.ViewModel
 {
-    internal class CalculatorViewModel
+    internal class CalculatorViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
         private string _numberDisplay;
         private double _inputNumber;
         private string _operation;
         private bool _readyNumber;
+        private double _secondNumber;
+        Calculator calModel;
+
+        public string NumberDisplay
+        {
+            get { return _numberDisplay; }
+            set
+            {
+                _numberDisplay = value;
+                OnPropertyChanged();
+            }
+        }
 
         public CalculatorViewModel()
         {
-            _numberDisplay = "0";
+            NumberDisplay = "0";
             _readyNumber = true;
+            calModel = new Calculator();
         }
 
         public void NumberPressed(string number)
         {
-            if (_readyNumber || _numberDisplay == "0")
+            if (_readyNumber || NumberDisplay == "0")
             {
-                _numberDisplay = number;
+                NumberDisplay = number;
                 _readyNumber = false;
             }
             else
             {
-                _numberDisplay += number;
+                NumberDisplay += number;
             }
         }
 
         public void OperationPressed(string operation)
         {
-            
+            _inputNumber = double.Parse(NumberDisplay);
+            _operation = operation;
+            _readyNumber = true;
         }
 
         public void EqualsPressed()
         {
+            try
+            {
+                if(!_readyNumber)
+                {
+                    _secondNumber = double.Parse(NumberDisplay);
+                }
+
+                double result = 0;
+                switch (_operation)
+                {
+                    case "＋":
+                        result = calModel.Add(_inputNumber, _secondNumber);
+                        break;
+                    case "－":
+                        result = calModel.Subtract(_inputNumber, _secondNumber);
+                        break;
+                    case "×":
+                        result = calModel.Multiply(_inputNumber, _secondNumber);
+                        break;
+                    case "÷":
+                        result = calModel.Divide(_inputNumber, _secondNumber);
+                        break;
+                }
+                NumberDisplay = result.ToString();
+                _inputNumber = result;
+                _readyNumber = true;
+            }
+            catch (Exception ex) 
+            {
+                    Console.WriteLine(ex.Message);
+            }
 
         }
 
         public void ClearPressed()
         {
-
+            NumberDisplay = "0";
+            _inputNumber = 0;
+            _operation = null;
+            _readyNumber = true;
         }
     }
 
